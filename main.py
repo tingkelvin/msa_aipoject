eebd105129e4bfcd8732e2e41b2e9cfbdeef22da
Kelvin Ting <ting_kelvin@icloud.com>
2021-07-31 16:51:07 +0930

    Implemment a simple LSTM model

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import datamodules
 import model
+
+
 #load data
 data_train, X_train, Y_train = datamodules.loadXML(
     "./data/Restaurants_Train.xml",
@@ -9,7 +11,7 @@ data_test, X_test, Y_test = datamodules.loadXML(
     "./data/Restaurants_Test_Gold.xml",
     polarities=['negative', 'neutral', 'positive'])
 
-#convert every word in a sentence to index
+#convert every word to index
 word2index, X_train_indices, X_test_indices, X_train_wl, X_train_wr, X_test_wl, X_test_wr = datamodules.word2index(
     X_train, X_test, data_train, data_test)
 
@@ -19,5 +21,17 @@ embed_vector_len = gloVec['the'].shape[0]
 vocab_len = len(word2index)
 
 #building an embedding layer for every word from our data 
-emb_matrix = model.getEmbedding(word2index,vocab_len,embed_vector_len,gloVec)
+emb_matrix = model.getEmbeddingMatrix(word2index,vocab_len,embed_vector_len,gloVec)
+embedding = model.getEmbedding(vocab_len, embed_vector_len, emb_matrix)
+
+#build a vanilla LSTM model
+vanillaLSTM = model.vanillaLSTM(embedding=embedding, input_shape=((69,)))
+
+#training a model
+model.train(vanillaLSTM, X_train_indices, Y_train, X_test_indices, Y_test)
+
+#plot the trained model accuracy
+model.plot(vanillaLSTM, "Vanilla LSTM")
+
+
 
--- model.py
+++ model.py
@@ -1,8 +1,16 @@
 import numpy as np
+from keras.layers.embeddings import Embedding
+from keras.layers import LSTM, Dropout, Dense, Input
+from keras.models import Model
+from keras.optimizers import Adam
+import tensorflow as tf
+import matplotlib.pyplot as plt
 
-def getEmbedding(word2index,vocab_len,embed_vector_len,gloVec):
+tf.random.set_seed(1234)
+adam = Adam(learning_rate = 0.001)
+
+def getEmbeddingMatrix(word2index,vocab_len,embed_vector_len,gloVec):
     emb_matrix = np.zeros((vocab_len+1, embed_vector_len))
-   
     for word, index in word2index.items():
         split_word = word.split()
         if len(split_word) == 1:
@@ -22,3 +30,48 @@ def getEmbedding(word2index,vocab_len,embed_vector_len,gloVec):
             else:
                 emb_matrix[index, :] = average_embedding
     return emb_matrix
+
+def getEmbedding(vocab_len, embed_vector_len, emb_matrix):
+    return Embedding(input_dim=vocab_len+1, output_dim=embed_vector_len,input_length=69, weights = [emb_matrix], trainable=False)
+
+def vanillaLSTM(embedding, input_shape):
+    X_indices = Input(input_shape)
+    embeddingLayer = embedding(X_indices)
+    X = LSTM(128)(embeddingLayer)
+    X = Dropout(0.2)(X)
+    X = Dense(3, activation='softmax')(X)
+    model = Model(inputs=X_indices, outputs=X)
+    return model
+
+def train(model, X_train_indices, Y_train, X_test_indices, Y_test):
+    model.compile(optimizer=adam, loss='categorical_crossentropy', metrics=['accuracy'])
+    model.fit(X_train_indices, Y_train, batch_size=64, epochs=10, validation_data=(X_test_indices, Y_test))
+
+def plot(model, title):
+    plt.plot(model.history.history['accuracy'])
+    plt.plot(model.history.history['val_accuracy'])
+    plt.title(title+' accuracy')
+    plt.ylabel('accuracy')
+    plt.xlabel('epoch')
+    plt.legend(['train', 'test'], loc='upper left')
+    plt.show()
+    
+    plt.plot(model.history.history['loss'])
+    plt.plot(model.history.history['val_loss'])
+    plt.title(title+' loss')
+    plt.ylabel('loss')
+    plt.xlabel('epoch')
+    plt.legend(['train', 'test'], loc='upper left')
+    plt.show()
+    
+    ##m_epoch = 0
+    ##max_accuracy = 0
+    ##max_val_accuracy = 0
+    ##for i in range(len(model.history.history['val_accuracy'])):
+       ## if model.history.history['val_accuracy'][i] > max_val_accuracy:
+       ##     m_epoch = i
+       ##     max_val_accuracy = model.history.history['val_accuracy'][i]
+       ##     max_accuaracy = model.history.history['accuracy'][i]
+    ##max_accuracy = round(max_accuracy* 100,2)
+    ##max_val_accuracy = round(max_val_accuracy* 100,2)
+    ##print(f"Model accuracy maximized at epoch", m_epoch,",", "Test data acccuracy:", max_val_accuracy, "%")
